1a54612ec8658170e5d112e6bac6ec801a968216
Alfredo Menezes <>
2023-03-01 18:28:15 +0000

    Initial commit

---
+++ teste_1.py
@@ -0,0 +1,9 @@
+print('Digite os números solicitados abaixo')
+n_1 = input('Número 1: ')
+n_2 = input('Número 2: ')
+
+n_int_1 = int(n_1)
+n_int_2 = int(n_2)
+
+print(f'A soma dos números é: {n_int_1 + n_int_2}')
+print(f'A média aritmética dos números é: {(n_int_1 + n_int_2) / 2}')
